83db670f8aed9711b8a4098d3642dd5b0966e09b
Rasel <mdraselkibria11@gmail.com>
2023-05-11 17:59:17 +0600

    express js assignment done,all requarments

--- book-collection/router.js
+++ book-collection/router.js
@@ -13,27 +13,41 @@ router.get('/books', (req, res) => {
 
 // Initialize an empty array to hold the collection of books
 const books=[]
-router.post("/add-book",(req,res)=>{
+router.post("/books",(req,res)=>{
     try{
         const id =uuidv4();
-        const {title,author}= req.body
-        const addBook = {
+        const {title,author,publishedDate}= req.body
+        const newBook = {
             id,
             title,
-            author
+            author,
+            publishedDate
         }
-        books.push(addBook)
-        res.status(200).json({msg:"Book add successfully",addBook})
+        books.push(newBook)
+        res.status(200).json({msg:"Book add successfully",newBook})
     }catch (err){
         res.status(400).json({msg:"Book add fail"})
     }
 })
 
-//delete book by id
-router.delete("/delete/:id",(req,res)=>{
-    const bookId = req.params.id; //get id using params
-    books.filter(book => book.id !== bookId);
-    res.json({ message: `Book with ID ${bookId} has been deleted.` });
+//delete book by id #method=delete
+router.delete("/books/:id",(req,res)=>{
+    try{
+        const bookId = req.params.id;
+        // Find the index of the book in the array of books
+        const bookIndex = books.findIndex((book) => book.id === bookId);
+
+        if (bookIndex === -1) {
+            // If the book is not found, return a 404 error
+            res.status(404).json({message:"Book Not found by Id"})
+        } else {
+            // Remove the book from the array of books
+            books.splice(bookIndex, 1);
+            res.status(200).json({message:"Book deleted successfully"})
+        }
+    }catch (err)  {
+        res.json({err:err})
+    }
 })
 
 
