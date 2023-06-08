4ba7ba22c507a9b64fb3ce08688f9f4826054f03
Rasel <mdraselkibria11@gmail.com>
2023-06-08 12:45:39 +0600

    update query

--- module-10-assignment/src/middleware/authenticate.js
+++ module-10-assignment/src/middleware/authenticate.js
@@ -1,16 +1,22 @@
-
-const jwt = require('jsonwebtoken');
-const isAuthenticated = async (req, res, next) => {
-    const {authorization} = req.headers
-    try {
-        const token = authorization.split(' ')[1]
-        const  decoded = jwt.verify(token,process.env.JWT_SECRET)
-        const {username,userId} = decoded;
-        req.username=username;
-        req.userId=userId
-        next()
-    } catch (err) {
-        res.status(401).json({msg: "Unauthorized"})
-    }
-}
-module.exports = isAuthenticated
+/**
+ * Create an Express.js middleware function called authenticate that verifies the JWT token in the request headers using the
+ * 'jsonwebtoken' library. If the token is valid, it should call the next middleware function. Otherwise,
+ *  it should return a 401 Unauthorized error.
+ */
+const jwt = require("jsonwebtoken");
+const authenticate = async (req, res, next) => {
+  // The authorization header is a Bearer token that contains the user's JWT token.
+  const { authorization } = req.headers;
+  try {
+    const token = authorization.split(" ")[1];
+    //docoded token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { username, userId } = decoded;
+    req.username = username;
+    req.userId = userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Unauthorized" });
+  }
+};
+module.exports = authenticate;
--- module-10-assignment/src/router/router.js
+++ module-10-assignment/src/router/router.js
@@ -1,11 +1,37 @@
 const router = require("express").Router();
-const {
-  createProduct,
-  findProducts,
-} = require("../controller/ProductController");
-//find product
-router.get("/products", findProducts);
-//create product
-router.post("/product", createProduct);
+const jwt = require("jsonwebtoken");
+const Product = require("../model/ProductModel");
+/*
+ Create an Express.js route that handles a GET request to '/products' and returns a JSON response
+  containing all products from the Mongoose "Product" collection. Ensure that the response includes 
+  only the name and price fields for each product.
+ */
 
+router.get("/products", async (req, res) => {
+  try {
+    let query = {};
+    let spacificField = { name: 1, price: 1, _id: 0 };
+    const products = await Product.find(query, spacificField);
+
+    res.status(200).json({ message: "Product get ", products: products });
+  } catch (error) {
+    res.status(404).json({ message: "Product not found", error });
+  }
+});
+
+/*
+Write a function called generateToken that generates a JWT token using the 'jsonwebtoken' library. 
+The function should accept a user ID and a secret key as parameters and return the generated token.
+*/
+exports.generateToken = (userId, secret) => {
+  // Create a JSON object with the user ID and expiration time
+  const claims = {
+    userId,
+    expires: new Date().getTime() + 1000 * 60 * 60 * 24, // 1 day
+  };
+  // Sign the claims with the secret key
+  const token = jwt.sign(claims, secret);
+  // Return the token
+  return token;
+};
 module.exports = router;
